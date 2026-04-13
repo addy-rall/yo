832a55d5618153acb2cfdcb721db0c7d78183950
addyral <199353199+addy-rall@users.noreply.github.com>
2026-04-13 09:25:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import CursorFX from './components/Cursorfx'
+
 import Hero from './components/Hero'
 import WhatIsTedx from './components/WhatIsTedx'
 import FeaturedSpeakers from './components/FeaturedSpeakers'
@@ -41,7 +41,7 @@ export default function App() {
     <BrowserRouter>
       <div className="app">
         <Navbar />
-        <CursorFX />
+        
         <Routes>
           <Route path="/" element={<Home />} />
          <Route path="/tedxbbau2025" element={<TEDx2025 />} />
